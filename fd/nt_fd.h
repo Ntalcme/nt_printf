063565526d21fcfb40acbd17ea19e0ec8187a2e3
ntalcme <ntalcme@gmail.com>
2025-03-31 04:17:28 +0200

    add nt_putstr_fd declaration

--- fd/nt_fd.h
+++ fd/nt_fd.h
@@ -5,7 +5,9 @@
 #define ERROR_WRITING -1
 
 # include <unistd.h>
+# include "../str_utils/nt_str_utils.h"
 
 int nt_putchar_fd(const char c, const int fd);
+int nt_putstr_fd(const char *str, const int fd);
 
 #endif
