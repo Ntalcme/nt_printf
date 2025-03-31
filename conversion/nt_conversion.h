c366f6cbb83e81c22e0b89bd3e1039babae31aed
ntalcme <ntalcme@gmail.com>
2025-03-31 05:01:42 +0200

    add declarations

--- conversion/nt_conversion.h
+++ conversion/nt_conversion.h
@@ -2,8 +2,10 @@
 #define NT_CONVERSION
 
 # include <stdlib.h>
+# include "../str_utils/nt_str_utils.h"
 
 #define MIN_INT -2147483648
+#define MIN_INT_STR "-2147483648"
 
 char nt_itochar(const int n);
 
