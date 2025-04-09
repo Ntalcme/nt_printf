8d696f7d981d15ff2c8ca54554f7da077c3ce8d8
ntalcme <ntalcme@gmail.com>
2025-04-09 12:00:17 +0200

    add license in the code

--- nt_printf.c
+++ nt_printf.c
@@ -1,3 +1,24 @@
+/*
+** nt_printf - Custom implementation of printf
+**
+** Copyright (c) 2025 Ntalcme
+**
+** This software is licensed under the MIT License.
+** You may obtain a copy of the License at:
+** https://opensource.org/licenses/MIT
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the conditions stated in the LICENSE file.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+*/
+
 # include "nt_printf.h"
 
 static ssize_t nt_print_nbr_base(char *base, long long number)
@@ -27,7 +48,7 @@ static ssize_t nt_print_nbr_base(char *base, long long number)
         res++;  
     }
 
-    else 
+    else    
     {
         tmp = nt_putchar_fd(base[number], STDOUT_FILENO);
         if (tmp == ERROR_WRITING) return (ERROR_WRITING);
